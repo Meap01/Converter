84b48069d1b0bbd7b9da4daf69643b1b202c0fd6
Daniel Krasovski <dkrasovskijj@gmail.com>
2019-01-07 10:53:09 +0000

    Initial commit

---
+++ program.c
@@ -0,0 +1,18 @@
+/* a fast progam, used to convert numbers.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int menu;
+	float celcius;
+	float fahrenheit;
+	float meter;
+	float inches;
+	float feet;
+
+	
+
+
+
+}
